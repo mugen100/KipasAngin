2f8d1df6a76e92a7626921367d5fb20fe71a2bbf
mugen100 <laptop.ideapads130@gmail.com>
2025-05-28 13:08:09 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,8 +3,8 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "sypezo.workers.dev"; // Ganti dengan domain utama kalian
-const serviceName = "anginkipas"; // Ganti dengan nama workers kalian
+const rootDomain = "m1r41mugen.my.id"; // Ganti dengan domain utama kalian
+const serviceName = "server"; // Ganti dengan nama workers kalian
 const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
